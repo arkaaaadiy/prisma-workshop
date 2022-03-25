ea86869255cefeb98ce0b628d4836d386f21dfdd
Ildar Nurgaleev <ildar.nyr@gmail.com>
2022-03-25 18:36:16 +0500

    try all crud operation and agregation

--- script.ts
+++ script.ts
@@ -4,7 +4,25 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+  // const allUsers = await getAllUsers()
+
+  // const createdUser = await createUser()
+
+  // const updatedUser = await updateUser()
+
+  // const result = await createPost()
+
+  // const result = await updatePost()
+
+  // const result = await getUserById()
+  
+  // const result = await getPostWithUser()
+
+  // const result = await createUserAndRelatedPost()
+
+  // const result = await getFilteredUser()
+
+  console.log('result', result)
 }
 
 main()
@@ -14,3 +32,94 @@ main()
   .finally(async () => {
     await prisma.$disconnect()
   })
+
+async function getFilteredUser() {
+  return await prisma.user.findMany({
+    where: {
+      name: {
+        startsWith: 't'
+      }
+    }
+  })
+}
+
+async function createUserAndRelatedPost() {
+  return await prisma.user.create({
+    data: {
+      email: 'test@qwe.rty',
+      name: 'qwe',
+      posts: {
+        create: {
+          title: 'user qwe title post',
+          content: 'qwerty use pencel a lot'
+        }
+      }
+    }
+  })
+}
+
+async function getPostWithUser() {
+  return await prisma.post.findMany({
+    include: {
+      author: true
+    }
+  })
+}
+
+async function getUserById() {
+  return await prisma.user.findMany({
+    where: {
+      id: 1
+    },
+    select: {
+      id: true,
+      name: true
+    }
+  })
+}
+
+async function updatePost() {
+  return await prisma.post.update({
+    where: {
+      id: 1
+    },
+    data: {
+      author: {
+        connect: { id: 3 }
+      }
+    }
+  })
+}
+
+async function createPost() {
+  return await prisma.post.create({
+    data: {
+      title: '123'
+    }
+  })
+}
+
+async function getAllUsers() {
+  return await prisma.user.findMany()
+}
+
+async function createUser() {
+  return await prisma.user.create({
+    data: {
+      email: '123@tr.ru',
+      name: 'create with Prisma'
+    }
+  })
+}
+
+async function updateUser() {
+  return await prisma.user.update({
+    data: {
+      name: '123'
+    },
+    where: {
+      id: 4
+    }
+  })
+}
+
